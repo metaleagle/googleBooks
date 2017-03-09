9ac9733b5c6556a9c4a0f4ac7754be1b18188664
Андрей Данишевский <asdanishevskiy@gmail.com>
2017-03-09 15:20:54 +0200

    Initial Commit

---
+++ GoogleBooksTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoogleBooksTest
+//
+//  Created by Андрей Данишевский on 09.03.17.
+//  Copyright © 2017 MetalEaglE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
